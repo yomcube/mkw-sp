65c5ea29038d5afdbef05d812d9973f73b4fa052
Aiden <86704247+vabold@users.noreply.github.com>
2024-04-03 03:18:31 -0400

    Unlock everything

---
+++ payload/game/system/SaveManager.cc
@@ -0,0 +1,9 @@
+#include "SaveManager.hh"
+
+namespace System {
+
+bool UnlockFlags_get() {
+    return true;
+}
+
+} // namespace System
---
+++ payload/game/system/SaveManager.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Common.hh>
+
+namespace System {
+
+REPLACE bool UnlockFlags_get();
+
+} // namespace System
