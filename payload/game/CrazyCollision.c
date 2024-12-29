7e9d83d8984e36ae9bff156db02c3829cd528c8b
TomCube <121310847+yomcube@users.noreply.github.com>
2024-12-28 23:44:41 -0600

    add semicolon

--- payload/game/CrazyCollision.c
+++ payload/game/CrazyCollision.c
@@ -1,10 +1,11 @@
 #include <Common.h>
 
 __attribute__((section("patches"))) const Patch patch_0x80441e68_0xB = {
-    .type = PATCH_TYPE_WRITE,
-    .write = {
-        (void *)(0x80441e68 + 0xB),
-        &(u32){(0x38A00001)},
-        sizeof(u32),
-    },
-}
+        .type = PATCH_TYPE_WRITE,
+        .write =
+                {
+                        (void *)(0x80441e68 + 0xB),
+                        &(u32){(0x38A00001)},
+                        sizeof(u32),
+                },
+};
