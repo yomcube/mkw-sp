e1acd27fe11c6580ce44437d828fa4df29af97b5
TomCube <121310847+yomcube@users.noreply.github.com>
2024-12-28 23:40:48 -0600

    use cast

--- payload/game/CrazyCollision.c
+++ payload/game/CrazyCollision.c
@@ -3,7 +3,7 @@
 __attribute__((section("patches"))) const Patch patch_0x80441e68_0xB = {
     .type = PATCH_TYPE_WRITE,
     .write = {
-        &(0x80441e68 + 0xB),
+        (void *)(0x80441e68 + 0xB),
         &(u32){(0x38A00001)},
         sizeof(u32),
     },
