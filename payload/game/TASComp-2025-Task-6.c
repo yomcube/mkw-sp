d2a0e596caea8b535f685f0a49230cc9887bedf3
TomCube <121310847+yomcube@users.noreply.github.com>
2025-03-22 12:10:10 -0500

    test

--- payload/game/TASComp-2025-Task-6.c
+++ payload/game/TASComp-2025-Task-6.c
@@ -1,7 +1,7 @@
 #include <Common.h>
 
 // Charge Mini-Turbos While Airborne [Gaberboo]
-PATCH_U32(TASComp2025Task6_1A, 0x418100ec);
+PATCH_U32(TASComp2025Task6_1A, 0, 0x418100ec);
 
 // IDBs Have MK8 Inward Drift [Gaberboo]
-PATCH_U32(TASComp2025Task6_2B, 0x38a00001);
+PATCH_U32(TASComp2025Task6_2B, 0, 0x38a00001);
